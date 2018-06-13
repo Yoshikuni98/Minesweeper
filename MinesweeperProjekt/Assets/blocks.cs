166c286b2aa0d498fea811cb40b92fc3fade07b5
Unknown <johnnyebel@yahoo.de>
2018-06-13 20:11:32 +0200

    Pasted Folder

---
+++ MinesweeperProjekt/Assets/blocks.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blocks
+{
+    int sizeH = 0;
+    int sizeW = 0;
+    bool isBomb = false;
+
+
+    public blocks()
+    {
+
+    }
+    public blocks(bool _isBomb)
+    {
+
+    }
+}
