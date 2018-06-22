b188bd4185977671e7b2d4f4f224ba137d0aedb2
Unknown <johnnyebel@yahoo.de>
2018-06-22 18:47:41 +0200

    dont fuck it up again pls

---
+++ MinesweeperProjekt/Assets/Scripts/Blocks.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocks {
+
+    bool isBomb;
+    int number;
+    bool isPressed;
+    bool isFlaged;
+
+    public Blocks(bool _isBomb, int _number, bool _isPressed, bool _isFlaged)
+    {
+        isBomb = _isBomb;
+        number = _number;
+        isPressed = _isPressed;
+        isFlaged = _isFlaged;
+    }
+
+
+
+}
---
+++ MinesweeperProjekt/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameController : MonoBehaviour {
+    [SerializeField]
+    private int Zahl = 0;
+    public int sizeH = 9;
+    public int sizeW = 9;
+    public Button Smiley_Normal;
+    public Button Smiley_Erstaunt;
+    public Button Smiley_Cool;
+    public Button Smiley_Traunrig;
+    public GameObject BackgroundPlane;
+
+    void Start()
+    {
+        BackgroundPlane.transform.localScale = new Vector3(sizeW, transform.localScale.y, sizeH+3);
+
+    }
+
+}
