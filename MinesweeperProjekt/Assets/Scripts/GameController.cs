64f5cf2981b5fe1491a05d08db0cc74c851d38e1
Unknown <johnnyebel@yahoo.de>
2018-06-25 21:25:16 +0200

    i dont know

--- MinesweeperProjekt/Assets/Scripts/GameController.cs
+++ MinesweeperProjekt/Assets/Scripts/GameController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class GameController : MonoBehaviour {
     public int sizeH = 9;
     public int sizeW = 9;
@@ -14,12 +13,12 @@ public class GameController : MonoBehaviour {
     public GameObject cube;
     public GameObject plane;
     public Camera maincamera;
+    float cameraZ;
     
 
     void Start()
     {
         GridGen();
-        
     }
 
     void Update()
@@ -42,22 +41,28 @@ public class GameController : MonoBehaviour {
             for (int x = 0; x < sizeW; x++)
             {
                 GameObject planes = Instantiate(plane);
-                planes.transform.position += new Vector3(x, y, -0.5f);
+                planes.transform.position += new Vector3(x, y, -0.55f);
                 planes.transform.SetParent(transform);
             }
         }
 
         float cameraX = sizeW / 2;
         float cameraY = sizeH / 2;
-        float cameraZ = cameraX * cameraY;
+        if (sizeH <= sizeW)
+        {
+            float cameraZ = sizeW;
+        }
 
         //16x16 Feld - Position muss Coordinate (7.5, 7.5, -28) haben
         //15x15 Feld - Position muss Coordinate (7, 7, -26) haben
+        //14x14 Feld - Position muss Coordinate (6.5, 6.5, -24.5) haben
         //Problem nur bei Gerade Feldern
         //ungerade genau in Mitte       z achse nicht richtig
 
-        maincamera.transform.position = new Vector3(cameraX,cameraY,-cameraZ);
-
+        maincamera.transform.position = new Vector3(cameraX,cameraY,maincamera.transform.position.z);
+        maincamera.orthographicSize = 14;
     }
+
+    
 
 }
---
+++ MinesweeperProjekt/Assets/Scripts/Plane.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour {
+
+    public Sprite standardPlane;
+    public Sprite flagPlane;
+
+    void Start()
+    {
+        MouseClickRight();
+    }
+
+	void OnMouseDown () {
+        Destroy(gameObject);
+	}
+
+    void MouseClickRight()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            
+        }
+        else
+        {
+            
+        }
+    }
+}
